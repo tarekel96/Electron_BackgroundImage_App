aa6674d2a1da68eae2e06d41398b85b6971cec46
Tarek <elhaj102@mail.chapman.edu>
2021-02-12 16:47:29 -0800

    created the navbar navitems and icon components and the home page

--- app/src/App.js
+++ app/src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import React from 'react';
+import Home from './pages/Home.js';
+import { HashRouter as Router, Route } from 'react-router-dom';
+import { Layout } from './components/Layout.js';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	return (
+		<Router>
+			<Layout>
+				{/* A <Switch> looks through its children <Route>s and
+          renders the first one that matches the current URL. */}
 
+				<Route path="/" component={Home} exact />
+				<Route path="/download" component={Download} exact />
+				<Route path="/pricing" component={Pricing} exact />
+				<Route path="/support" component={Support} exact />
+			</Layout>
+		</Router>
+	);
+};
+const Download = () => {
+	return <h2>Download</h2>;
+};
+const Pricing = () => {
+	return <h2>Pricing</h2>;
+};
+const Support = () => {
+	return <h2>Support</h2>;
+};
 export default App;
---
+++ app/src/components/Icon.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import styles from './Icon.module.css';
+
+export const Icon = () => {
+	return (
+		<section className={styles['navIcon']}>
+			<SVGIcon />
+			<h2 className={styles['navIconTitle']}>Screensaver</h2>
+		</section>
+	);
+};
+const SVGIcon = () => {
+	return (
+		<svg width="49" height="24" viewBox="0 0 49 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+			<path
+				fillRule="evenodd"
+				clipRule="evenodd"
+				d="M14.4295 23.4814L48.6992 23.4814L48.699 23.4813L48.699 7.63508L34.2696 0.962923L34.2696 16.8093L34.2697 16.8093L0.000198364 16.8093L14.4295 23.4814Z"
+				fill="black"
+			/>
+		</svg>
+	);
+};
