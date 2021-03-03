08e084b1b4c32482a40517d5ce3380479ff8b72c
DomCastaneda <dcastaneda2828@gmail.com>
2021-03-02 23:00:19 -0800

    Added UserSettings Form

--- app/src/App.js
+++ app/src/App.js
@@ -3,9 +3,11 @@ import Home from './pages/Home.js';
 import Test from './pages/Test.js';
 import Download from './pages/Download.js';
 import UserHome from './pages/UserHome.js';
+import Settings from './pages/UserSettings.js';
 import { HashRouter as Router, Route } from 'react-router-dom';
 import { Layout } from './components/Layout.js';
 import './styles/main.css';
+import UserSettings from './pages/UserSettings.js';
 
 const App = () => {
 	return (
@@ -17,6 +19,7 @@ const App = () => {
 				<Route path="/support" component={Support} exact />
 				<Route path="/test" component={Test} exact />
 				<Route path="/userhome" component={UserHome} />
+				<Route path="/usersettings" component={UserSettings} exact/>
 			</Layout>
 		</Router>
 	);
---
+++ app/src/pages/UserSettings.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const UserSettings = () => {
+	return (
+		<div>
+			<h2>User Settings</h2>
+		</div>
+	);
+};
+
+export default UserSettings;
