76502a56efcc25f05fa4420513518c2f89283abd
DomCastaneda <dcastaneda2828@gmail.com>
2021-03-02 20:46:02 -0800

    Added Footer Component

--- app/src/pages/Test.js
+++ app/src/pages/Test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { SideNavBar } from '../components/SideNavBar.js';
 import { Button } from '../components/Button.js';
+import { Copyright, Footer } from '../components/Footer.js';
 
 const Test = () => {
 	return (
@@ -8,6 +9,8 @@ const Test = () => {
 			<SideNavBar />
 			<h1>Test Page</h1>
 			<Button variant="secondary">Click Here</Button>
+			<Footer />
+			<Copyright />
 		</div>
 	);
 };
