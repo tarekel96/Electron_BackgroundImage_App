d78a467976a097cd58fb282a196bf518fb5394b3
tarekel96 <elhaj102@mail.chapman.edu>
2021-03-14 20:03:11 -0700

    added a notfound page for invalid routes and urls

--- web/src/App.js
+++ web/src/App.js
@@ -4,7 +4,7 @@ import Download from './pages/Download.js';
 import Pricing from './pages/Pricing.js';
 import ContactUs from './pages/ContactUs.js';
 import NotFound from './pages/NotFound.js';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Layout } from './components/Layout.js';
 import './styles/main.css';
 
@@ -12,10 +12,13 @@ const App = () => {
 	return (
 		<Router>
 			<Layout>
-				<Route path="/" component={Home} exact />
-				<Route path="/download" component={Download} exact />
-				<Route path="/pricing" component={Pricing} exact />
-				<Route path="/contact" component={ContactUs} exact />
+				<Switch>
+					<Route path="/" component={Home} exact />
+					<Route path="/download" component={Download} exact />
+					<Route path="/pricing" component={Pricing} exact />
+					<Route path="/contact" component={ContactUs} exact />
+					<Route component={NotFound} />
+				</Switch>
 			</Layout>
 		</Router>
 	);
