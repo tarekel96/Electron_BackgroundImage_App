2bd0fe3af370d24c6f845ece8bf55a110773b4f8
DomCastaneda <dcastaneda2828@gmail.com>
2021-03-25 20:35:20 -0700

    Added lazy loading image to home page
    Lazy load package from https://www.npmjs.com/package/react-lazy-load-image-component
    
    Pricing page lazy loading not working yet

--- web/src/pages/Pricing.js
+++ web/src/pages/Pricing.js
@@ -3,15 +3,18 @@ import { Typography } from '../components/Typography.js';
 import { SectionImageLeft } from '../components/SectionImage.js';
 import Variants from '../styles/Variants.js';
 import styles from './pricing.module.css';
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import 'react-lazy-load-image-component/src/effects/blur.css';
 const { H_4, PARAGRAPH } = Variants;
 
 const Pricing = () => {
 	return (
 		<section className={styles['pricingLayoutContainer']}>
 			<section className={styles['pricingContainer']}>
-				<div>
-					<SectionImageLeft />
-				</div>
+			<LazyLoadImage
+					src={SectionImageLeft}
+    				alt={'Section Image'}
+   			 		effect="blur"/>
 				<div>
 					<Typography variant={H_4}>Pricing</Typography>
 					<Typography variant={PARAGRAPH}>
