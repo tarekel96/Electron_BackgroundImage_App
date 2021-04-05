14e0538174955b6cd0bae942d3b3ccff3e5d0cb9
Christopher Chang <chiggiechang@gmail.com>
2021-04-05 02:33:13 -0700

    Create very basic screensaver display functionality w/ basic Instagram
    There's only the bare minimum of front-end stuff right now. And there
    are some potential security concerns with our use of the Instagram Basic
    Display API, namely with how we use the App Secret and with our reliance
    on nodeIntegration: true and contextIsolation: false.

--- screensaver-display/src/App.js
+++ screensaver-display/src/App.js
@@ -1,14 +1,62 @@
 import TempSettings from "./pages/TempSettings";
+import Auth from "./pages/Auth";
  // Use HashRouter (as opposed to BrowserRouter) because it works better with file-based Electron
 import { HashRouter as Router, Route } from "react-router-dom";
+import { useEffect, useState } from "react";
+
+const { ipcRenderer } = window.require("electron");
 
 function App() {
   return (
     <Router>
-      {/* <Route path="/" component={TempSettings} exact /> */}
+      <Route path="/" component={TempIndex} exact />
       <Route path="/temp_settings" component={TempSettings} exact />
+      <Route path="/auth" component={Auth} />
     </Router>
   );
+}
+
+function TempIndex() {
+  const [postsInfo, setPostsInfo] = useState([]);
+  const [postIndex, setPostIndex] = useState(0);
+
+  useEffect(() => {
+    const existingInfo = ipcRenderer.sendSync("read-posts-info");
+    if (existingInfo === null) {
+      return;
+    }
+
+    console.log(existingInfo);
+    setPostsInfo(existingInfo);
+
+    const interval = setInterval(() => {
+      if (postIndex >= postsInfo.length - 1) {
+        setPostIndex(0);
+        // console.log("Reset postIndex to 0.");
+      } else {
+        setPostIndex(postIndex + 1);
+        // console.log("Incremented postIndex!");
+      }
+
+      // console.log("postIndex was most recently: " + postIndex);
+    }, 2000);
+
+    return () => clearInterval(interval);
+  }, [postIndex, postsInfo.length]);
+
+  console.log(postsInfo);
+
+  const currentImage = postsInfo.length > 0 ? (
+    <img src={postsInfo[postIndex].media_url} alt=""></img>
+  ) : (
+    <h1>Oops! You don't have any images available. Try logging in with the settings app.</h1>
+  );
+
+  return (
+    <div>
+      {currentImage}
+    </div>
+  )
 }
 
 export default App;
--- screensaver-display/src/pages/TempSettings.js
+++ screensaver-display/src/pages/TempSettings.js
@@ -1,11 +1,75 @@
-import React from 'react'
+import { React, useEffect, useState } from "react";
+import ky from "ky";
+
+const { ipcRenderer } = window.require("electron");
 
 const TempSettings = () => {
+  const [authToken, setAuthToken] = useState(null);
+  const [postsInfo, setPostsInfo] = useState(null);
+
+  useEffect(() => {
+    if (authToken === null) {
+      const existingToken = ipcRenderer.sendSync("ig-bd-read-token");
+      if (existingToken === null) {
+        return;
+      }
+
+      setAuthToken(existingToken);
+
+      (async () => {
+        const userInfo = await ky.get("https://graph.instagram.com/me/media", {
+          searchParams: {
+            fields: "id,caption,media_url,media_type,permalink",
+            access_token: existingToken,
+          },
+        });
+        if (!userInfo.ok) {
+          console.log(
+            "There's been a fetch error. Status code: " + userInfo.status
+          );
+          console.log("Status text: " + userInfo.statusText);
+          const responseText = await userInfo.text();
+          console.log("Response text: " + responseText);
+        }
+        const userInfoJson = await userInfo.json();
+
+        const latestPosts = userInfoJson.data
+          .filter((info) => info.media_type === "IMAGE")
+          .slice(0, 5);
+        setPostsInfo(latestPosts);
+
+        ipcRenderer.send("save-posts-info", latestPosts);
+      })();
+    }
+  }, [authToken, postsInfo]);
+
+  const mediaImages = postsInfo ? (
+    <div>
+      {postsInfo.map((info) => (
+        <img src={info.media_url} alt="" />
+      ))}
+    </div>
+  ) : null;
+
   return (
     <div>
       <h1>Settings (For Now)</h1>
+      <button onClick={LogInToInstagram}>Log in.</button>
+      <p style={{ wordWrap: "break-word" }}>
+        {authToken === null
+          ? "Instagram token: Not logged in"
+          : `Instagram token: ${authToken}`}
+      </p>
+      {mediaImages}
     </div>
-  )
+  );
+};
+
+function LogInToInstagram() {
+  const authURL =
+    "https://api.instagram.com/oauth/authorize?client_id=765093417767855&redirect_uri=https://localhost:3000/auth/&scope=user_profile,user_media&response_type=code";
+
+  window.location = authURL;
 }
 
 export default TempSettings;
