a4a74583592048ffe3d8053e4b94cb763a395e98
DomCastaneda <dcastaneda2828@gmail.com>
2021-04-04 22:07:57 -0700

    Added LazyLoading to SectionImage Component

--- web/src/components/SectionImage.js
+++ web/src/components/SectionImage.js
@@ -1,18 +1,28 @@
 import React from 'react';
 import DEFAULT_IMAGE from '../styles/ExampleImage2.jpg';
 import styles from './sectionimage.module.css';
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import 'react-lazy-load-image-component/src/effects/blur.css';
 
 export const SectionImageLeft = () => {
 	return (
             //<div className={styles['sectionImageBack']}>
-			<img className={styles['sectionImageLeft']} src={DEFAULT_IMAGE} alt="SectionImage" />
+			<LazyLoadImage
+					className={styles['sectionImageLeft']} 
+					src={DEFAULT_IMAGE}
+    				alt={'SectionImage'}
+   			 		effect="blur"/>
             //</div>
 	);
 };
 export const SectionImageRight = () => {
 	return (
             //<div className={styles['sectionImageBack']}>
-			<img className={styles['sectionImageRight']} src={DEFAULT_IMAGE} alt="SectionImage" />
+			<LazyLoadImage
+					className={styles['sectionImageRight']} 
+					src={DEFAULT_IMAGE}
+    				alt={'SectionImage'}
+   			 		effect="blur"/>
             //</div>
 	);
 };
--- web/src/pages/Pricing.js
+++ web/src/pages/Pricing.js
@@ -3,18 +3,13 @@ import { Typography } from '../components/Typography.js';
 import { SectionImageLeft } from '../components/SectionImage.js';
 import Variants from '../styles/Variants.js';
 import styles from './pricing.module.css';
-import { LazyLoadImage } from 'react-lazy-load-image-component';
-import 'react-lazy-load-image-component/src/effects/blur.css';
 const { H_4, PARAGRAPH } = Variants;
 
 const Pricing = () => {
 	return (
 		<section className={styles['pricingLayoutContainer']}>
 			<section className={styles['pricingContainer']}>
-			<LazyLoadImage
-					src={SectionImageLeft}
-    				alt={'Section Image'}
-   			 		effect="blur"/>
+			<SectionImageLeft />
 				<div>
 					<Typography variant={H_4}>Pricing</Typography>
 					<Typography variant={PARAGRAPH}>
