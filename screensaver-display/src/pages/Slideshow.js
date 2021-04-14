f8b2950c80d72071a6bbde1181118886a589b2b4
Tarek <elhaj102@mail.chapman.edu>
2021-04-13 18:51:49 -0700

    redirects now to settings if not logged in

--- screensaver-display/src/pages/Slideshow.js
+++ screensaver-display/src/pages/Slideshow.js
@@ -1,46 +1,57 @@
-import { useEffect, useState } from "react";
-import styles from "./slideshow.module.css";
+import { useEffect, useState } from 'react';
+import { Redirect } from 'react-router-dom';
+import styles from './slideshow.module.css';
 
-const { ipcRenderer } = window.require("electron");
+const { ipcRenderer } = window.require('electron');
 
 function Slideshow() {
-  const [postsInfo, setPostsInfo] = useState([]);
-  const [postIndex, setPostIndex] = useState(0);
-  const [cycleTime, setCycleTime] = useState(2000);
+	const [ postsInfo, setPostsInfo ] = useState([]);
+	const [ postIndex, setPostIndex ] = useState(0);
+	const [ cycleTime, setCycleTime ] = useState(2000);
 
-  useEffect(() => {
-    const existingInfo = ipcRenderer.sendSync("read-posts-info");
-    if (existingInfo === null) {
-      return;
-    }
+	useEffect(
+		() => {
+			const existingInfo = ipcRenderer.sendSync('read-posts-info');
+			if (existingInfo === null) {
+				return;
+			}
 
-    console.log(existingInfo);
-    setPostsInfo(existingInfo);
+			console.log(existingInfo);
+			setPostsInfo(existingInfo);
 
-    const interval = setInterval(() => {
-      if (postIndex >= postsInfo.length - 1) {
-        setPostIndex(0);
-        // console.log("Reset postIndex to 0.");
-      } else {
-        setPostIndex(postIndex + 1);
-        // console.log("Incremented postIndex!");
-      }
+			const interval = setInterval(() => {
+				if (postIndex >= postsInfo.length - 1) {
+					setPostIndex(0);
+					// console.log("Reset postIndex to 0.");
+				}
+				else {
+					setPostIndex(postIndex + 1);
+					// console.log("Incremented postIndex!");
+				}
 
-      // console.log("postIndex was most recently: " + postIndex);
-    }, cycleTime);
+				// console.log("postIndex was most recently: " + postIndex);
+			}, cycleTime);
 
-    return () => clearInterval(interval);
-  }, [postIndex, postsInfo.length, cycleTime]);
+			return () => clearInterval(interval);
+		},
+		[ postIndex, postsInfo.length, cycleTime ]
+	);
 
-  console.log(postsInfo);
+	console.log(postsInfo);
 
-  const currentImage = postsInfo.length > 0 ? (
-      <img src={postsInfo[postIndex].media_url} className={styles.center} alt=""></img>
-  ) : (
-    <h1>Oops! You don't have any images available. Try logging in with the settings app.</h1>
-  );
+	const currentImage =
+		postsInfo.length > 0 ? (
+			<img src={postsInfo[postIndex].media_url} className={styles.center} alt="" />
+		) : (
+			<Redirect
+				to={{
+					pathname: '/settings'
+				}}
+			/>
+			// <h1>Oops! You don't have any images available. Try logging in with the settings app.</h1>
+		);
 
-  return currentImage;
+	return currentImage;
 }
 
 export default Slideshow;
