8cd4cf518d68f6d9a9328874c05cbd9230d86901
Christopher Chang <chiggiechang@gmail.com>
2021-04-13 22:34:43 -0700

    Remove obsolete settings-app folder

--- settings-app/src/components/ThumbnailImage.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import DEFAULT_IMAGE from '../styles/ExampleImage2.jpg';
-import styles from './thumbnailimage.module.css';
-
-export const ThumbnailImage = () => {
-	return (
-			<img className={styles['thumbnailImage']} src={DEFAULT_IMAGE} alt="Thumbnail" />
-	);
-};
