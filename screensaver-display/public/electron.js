8e980e93d785f181682780fafbaaef690e816806
Tarek <elhaj102@mail.chapman.edu>
2021-04-13 18:26:42 -0700

    updated repo for IG

--- screensaver-display/public/electron.js
+++ screensaver-display/public/electron.js
@@ -1,99 +1,97 @@
-const { app, BrowserWindow, ipcMain } = require("electron");
-const path = require("path");
-const isDev = require("electron-is-dev");
-const FormData = require("form-data");
-const fetch = require("node-fetch").default;
-const fs = require("fs");
-const https = require("https");
+const { app, BrowserWindow, ipcMain } = require('electron');
+const path = require('path');
+const isDev = require('electron-is-dev');
+const FormData = require('form-data');
+const fetch = require('node-fetch').default;
+const fs = require('fs');
+const https = require('https');
 
-const shouldShowTempSettings = process.argv.includes("--settings");
-const urlBasis = isDev
-  ? "http://localhost:3000/"
-  : `file://${path.join(__dirname, "../build/index.html")}`;
+const shouldShowTempSettings = process.argv.includes('--settings');
+const urlBasis = isDev ? 'http://localhost:3000/' : `file://${path.join(__dirname, '../build/index.html')}`;
 
-const storagePath = app.getPath("appData") + "/shared-screensaver";
+const storagePath = app.getPath('appData') + '/shared-screensaver';
 
-const APP_SECRET = "a7a0947d0367a41024f825989bf65049";
+const APP_SECRET = 'a7a0947d0367a41024f825989bf65049';
 
 if (!fs.existsSync(storagePath)) {
-  fs.mkdirSync(storagePath);
+	fs.mkdirSync(storagePath);
 }
 
 function createWindow() {
-  const win = new BrowserWindow({
-    width: 800,
-    height: 600,
-    frame: false,
-    fullscreen: !shouldShowTempSettings,
-    webPreferences: {
-      nodeIntegration: true,
+	const win = new BrowserWindow({
+		width: 800,
+		height: 600,
+		// frame: false,
+		fullscreen: !shouldShowTempSettings,
+		webPreferences: {
+			nodeIntegration: true,
 
-      // This allows us to access window.require("electron"), but according to
-      // https://www.electronjs.org/docs/tutorial/context-isolation "every single
-      // application" should have contextIsolation enabled for security purposes
-      contextIsolation: false,
-    },
-  });
+			// This allows us to access window.require("electron"), but according to
+			// https://www.electronjs.org/docs/tutorial/context-isolation "every single
+			// application" should have contextIsolation enabled for security purposes
+			contextIsolation: false
+		}
+	});
 
-  win.webContents.on("will-navigate", redirectAuthenticate.bind(null, win));
+	win.webContents.on('will-navigate', redirectAuthenticate.bind(null, win));
 
-  // For Development: Run from localhost (background react server)
-  // For Builds: Run from index.html file in build/
-  win.loadURL(shouldShowTempSettings ? urlBasis + "#/settings_home" : urlBasis);
+	// For Development: Run from localhost (background react server)
+	// For Builds: Run from index.html file in build/
+	win.loadURL(shouldShowTempSettings ? urlBasis + '#/settings_home' : urlBasis);
 }
 
 app.whenReady().then(() => {
-  createWindow();
+	createWindow();
 
-  app.on("activate", () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow();
-    }
-  });
+	app.on('activate', () => {
+		if (BrowserWindow.getAllWindows().length === 0) {
+			createWindow();
+		}
+	});
 });
 
-app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") {
-    app.quit();
-  }
+app.on('window-all-closed', () => {
+	if (process.platform !== 'darwin') {
+		app.quit();
+	}
 });
 
-ipcMain.on("download", (event, arg) => {
-  const { url } = arg;
+ipcMain.on('download', (event, arg) => {
+	const { url } = arg;
 
-  console.log("Downloading an image!");
+	console.log('Downloading an image!');
 
-  const file = fs.createWriteStream("image.jpg");
-  const request = https.get(url, (dlResponse) => {
-    dlResponse.pipe(file);
-  });
+	const file = fs.createWriteStream('image.jpg');
+	https.get(url, (dlResponse) => {
+		dlResponse.pipe(file);
+	});
 });
 
-ipcMain.on("ig-bd-read-token", (event, arg) => {
-  const tokenFilePath = storagePath + "/IGBasicDisplayLongLivedToken";
+ipcMain.on('ig-bd-read-token', (event, arg) => {
+	const tokenFilePath = storagePath + '/IGBasicDisplayLongLivedToken';
 
-  try {
-    event.returnValue = fs.readFileSync(tokenFilePath, "utf8");
-  } catch (err) {
-    event.returnValue = null;
-  }
+	try {
+		event.returnValue = fs.readFileSync(tokenFilePath, 'utf8');
+	} catch (err) {
+		event.returnValue = null;
+	}
 });
 
-ipcMain.on("save-posts-info", (event, arg) => {
-  const latestPosts = arg;
-  const postsInfoPath = storagePath + "/IGBasicPostsInfo.json";
+ipcMain.on('save-posts-info', (event, arg) => {
+	const latestPosts = arg;
+	const postsInfoPath = storagePath + '/IGBasicPostsInfo.json';
 
-  fs.writeFileSync(postsInfoPath, JSON.stringify(latestPosts));
+	fs.writeFileSync(postsInfoPath, JSON.stringify(latestPosts));
 });
 
-ipcMain.on("read-posts-info", (event, arg) => {
-  try {
-    const postsInfoPath = storagePath + "/IGBasicPostsInfo.json";
+ipcMain.on('read-posts-info', (event, arg) => {
+	try {
+		const postsInfoPath = storagePath + '/IGBasicPostsInfo.json';
 
-    event.returnValue = require(postsInfoPath);
-  } catch (err) {
-    event.returnValue = null;
-  }
+		event.returnValue = require(postsInfoPath);
+	} catch (err) {
+		event.returnValue = null;
+	}
 });
 
 ipcMain.on('save-settings', (event, args) => {
@@ -101,96 +99,89 @@ ipcMain.on('save-settings', (event, args) => {
 });
 
 function redirectAuthenticate(win, event, newUrl) {
-  const API_AUTH_PATH = "https://localhost:3000/auth/";
-  const PAGE_AUTH_PATH = urlBasis + "#/auth";
+	const API_AUTH_PATH = 'https://localhost:3000/auth/';
+	const PAGE_AUTH_PATH = urlBasis + '#/auth';
 
-  // console.log("redirect newUrL: " + newUrl);
+	// console.log("redirect newUrL: " + newUrl);
 
-  if (newUrl.startsWith(API_AUTH_PATH)) {
-    win.loadURL("https://en.wikipedia.org/wiki/Main_Page"); // Load this while we asynchronously exchange for the short-lived user token
+	if (newUrl.startsWith(API_AUTH_PATH)) {
+		win.loadURL('https://en.wikipedia.org/wiki/Main_Page'); // Load this while we asynchronously exchange for the short-lived user token
 
-    const fetchTokens = async () => {
-      // console.log(`newUrl: ${newUrl}`);
-      const urlParams = new URLSearchParams(new URL(newUrl).search);
-      // console.log(`urlParams: ${urlParams.entries()}`);
-      const authorizationCode = urlParams.get("code");
+		const fetchTokens = async () => {
+			// console.log(`newUrl: ${newUrl}`);
+			const urlParams = new URLSearchParams(new URL(newUrl).search);
+			// console.log(`urlParams: ${urlParams.entries()}`);
+			const authorizationCode = urlParams.get('code');
 
-      /// Obtain short-lived token
-      const formData = new FormData();
-      formData.append("client_id", "765093417767855");
-      formData.append("client_secret", APP_SECRET);
-      formData.append("grant_type", "authorization_code");
-      formData.append("redirect_uri", API_AUTH_PATH);
-      formData.append("code", authorizationCode);
+			/// Obtain short-lived token
+			const formData = new FormData();
+			formData.append('client_id', '765093417767855');
+			formData.append('client_secret', APP_SECRET);
+			formData.append('grant_type', 'authorization_code');
+			formData.append('redirect_uri', API_AUTH_PATH);
+			formData.append('code', authorizationCode);
 
-      const response = await fetch(
-        "https://api.instagram.com/oauth/access_token",
-        {
-          method: "POST",
-          mode: "cors",
-          headers: {
-            "Access-Control-Allow-Origin": "*", // TODO(Chris): Unsafe, should change later
-            Origin: "https://localhost:3000",
-          },
-          body: formData,
-        }
-      );
+			const response = await fetch('https://api.instagram.com/oauth/access_token', {
+				method: 'POST',
+				mode: 'cors',
+				headers: {
+					'Access-Control-Allow-Origin': '*', // TODO(Chris): Unsafe, should change later
+					Origin: 'https://localhost:3000'
+				},
+				body: formData
+			});
 
-      if (!response.ok) {
-        console.log(
-          "There's been a fetch error. Status code: " + response.status
-        );
-        console.log("Status text: " + response.statusText);
-        const responseText = await response.text();
-        console.log("Response text: " + responseText);
+			if (!response.ok) {
+				console.log("There's been a fetch error. Status code: " + response.status);
+				console.log('Status text: ' + response.statusText);
+				const responseText = await response.text();
+				console.log('Response text: ' + responseText);
 
-        res.status(500);
-        return;
-      }
+				// response.status(500);
+				return;
+			}
 
-      const slData = await response.json();
-      console.log(JSON.stringify(slData));
-      // console.log(`Short-lived token from Electron: ${slData.access_token}`);
+			const slData = await response.json();
+			console.log(JSON.stringify(slData));
+			// console.log(`Short-lived token from Electron: ${slData.access_token}`);
 
-      /// Exchange short-lived token for long-lived token
-      const llResponse = await fetch(
-        `https://graph.instagram.com/access_token?grant_type=ig_exchange_token&client_secret=${APP_SECRET}&access_token=${slData.access_token}`,
-        {
-          method: "GET",
-        }
-      );
+			/// Exchange short-lived token for long-lived token
+			const llResponse = await fetch(
+				`https://graph.instagram.com/access_token?grant_type=ig_exchange_token&client_secret=${APP_SECRET}&access_token=${slData.access_token}`,
+				{
+					method: 'GET'
+				}
+			);
 
-      if (!llResponse.ok) {
-        console.log(
-          "There's been a fetch error. Status code: " + llResponse.status
-        );
-        console.log("Status text: " + llResponse.statusText);
-        const llResponseText = await llResponse.text();
-        console.log("Response text: " + llResponseText);
+			if (!llResponse.ok) {
+				console.log("There's been a fetch error. Status code: " + llResponse.status);
+				console.log('Status text: ' + llResponse.statusText);
+				const llResponseText = await llResponse.text();
+				console.log('Response text: ' + llResponseText);
 
-        res.status(500);
-        return;
-      }
+				// res.status(500);
+				return;
+			}
 
-      const llData = await llResponse.json();
-      // console.log(`Long-lived token from Electron: ${llData.access_token}`);
+			const llData = await llResponse.json();
+			// console.log(`Long-lived token from Electron: ${llData.access_token}`);
 
-      /// Store long-lived token in the file system
-      const tokenFilePath = storagePath + "/IGBasicDisplayLongLivedToken";
-      fs.writeFileSync(tokenFilePath, llData.access_token);
+			/// Store long-lived token in the file system
+			const tokenFilePath = storagePath + '/IGBasicDisplayLongLivedToken';
+			fs.writeFileSync(tokenFilePath, llData.access_token);
 
-      /// Redirect to React auth page, sending the long-lived token along
-      console.log(`Tried to navigate to ${API_AUTH_PATH}, so redirecting...`);
+			/// Redirect to React auth page, sending the long-lived token along
+			console.log(`Tried to navigate to ${API_AUTH_PATH}, so redirecting...`);
 
-      const redirectUrl = new URL(PAGE_AUTH_PATH);
-      redirectUrl.searchParams.append("code", authorizationCode);
-      redirectUrl.searchParams.append("ll_token", llData.access_token);
-      // console.log(`code: ${authorizationCode}`);
-      console.log("redirectUrl: " + redirectUrl);
+			const redirectUrl = new URL(PAGE_AUTH_PATH);
+			redirectUrl.searchParams.append('code', authorizationCode);
+			redirectUrl.searchParams.append('ll_token', llData.access_token);
+			// console.log(`code: ${authorizationCode}`);
+			console.log('redirectUrl: ' + redirectUrl);
 
-      win.loadURL(redirectUrl.href);
-    };
+			win.loadURL(redirectUrl.href);
+		};
 
-    fetchTokens(); // Asynchronous
-  }
+		fetchTokens(); // Asynchronous
+	}
 }
--- screensaver-display/src/pages/InstagramSettings.js
+++ screensaver-display/src/pages/InstagramSettings.js
@@ -1,75 +1,67 @@
-import { React, useEffect, useState } from "react";
-import ky from "ky";
+import { React, useEffect, useState } from 'react';
+import ky from 'ky';
 
-const { ipcRenderer } = window.require("electron");
+const { ipcRenderer, shell } = window.require('electron');
 
 const InstagramSettings = () => {
-  const [authToken, setAuthToken] = useState(null);
-  const [postsInfo, setPostsInfo] = useState(null);
+	const [ authToken, setAuthToken ] = useState(null);
+	const [ postsInfo, setPostsInfo ] = useState(null);
 
-  useEffect(() => {
-    if (authToken === null) {
-      const existingToken = ipcRenderer.sendSync("ig-bd-read-token");
-      if (existingToken === null) {
-        return;
-      }
+	useEffect(
+		() => {
+			if (authToken === null) {
+				const existingToken = ipcRenderer.sendSync('ig-bd-read-token');
+				if (existingToken === null) {
+					return;
+				}
 
-      setAuthToken(existingToken);
+				setAuthToken(existingToken);
 
-      (async () => {
-        const userInfo = await ky.get("https://graph.instagram.com/me/media", {
-          searchParams: {
-            fields: "id,caption,media_url,media_type,permalink",
-            access_token: existingToken,
-          },
-        });
-        if (!userInfo.ok) {
-          console.log(
-            "There's been a fetch error. Status code: " + userInfo.status
-          );
-          console.log("Status text: " + userInfo.statusText);
-          const responseText = await userInfo.text();
-          console.log("Response text: " + responseText);
-        }
-        const userInfoJson = await userInfo.json();
+				(async () => {
+					const userInfo = await ky.get('https://graph.instagram.com/me/media', {
+						searchParams: {
+							fields: 'id,caption,media_url,media_type,permalink',
+							access_token: existingToken
+						}
+					});
+					if (!userInfo.ok) {
+						console.log("There's been a fetch error. Status code: " + userInfo.status);
+						console.log('Status text: ' + userInfo.statusText);
+						const responseText = await userInfo.text();
+						console.log('Response text: ' + responseText);
+					}
+					const userInfoJson = await userInfo.json();
 
-        const latestPosts = userInfoJson.data
-          .filter((info) => info.media_type === "IMAGE")
-          .slice(0, 5);
-        setPostsInfo(latestPosts);
+					const latestPosts = userInfoJson.data.filter((info) => info.media_type === 'IMAGE').slice(0, 5);
+					setPostsInfo(latestPosts);
 
-        ipcRenderer.send("save-posts-info", latestPosts);
-      })();
-    }
-  }, [authToken, postsInfo]);
+					ipcRenderer.send('save-posts-info', latestPosts);
+				})();
+			}
+		},
+		[ authToken, postsInfo ]
+	);
 
-  const mediaImages = postsInfo ? (
-    <div>
-      {postsInfo.map((info) => (
-        <img src={info.media_url} alt="" />
-      ))}
-    </div>
-  ) : null;
+	const mediaImages = postsInfo ? <div>{postsInfo.map((info) => <img src={info.media_url} alt="" />)}</div> : null;
 
-  return (
-    <div>
-      <h1>Settings (For Now)</h1>
-      <button onClick={LogInToInstagram}>Log in.</button>
-      <p style={{ wordWrap: "break-word" }}>
-        {authToken === null
-          ? "Instagram token: Not logged in"
-          : `Instagram token: ${authToken}`}
-      </p>
-      {mediaImages}
-    </div>
-  );
+	return (
+		<div>
+			<h1>Settings (For Now)</h1>
+			<button onClick={LogInToInstagram}>Log in.</button>
+			<p style={{ wordWrap: 'break-word' }}>
+				{authToken === null ? 'Instagram token: Not logged in' : `Instagram token: ${authToken}`}
+			</p>
+			{mediaImages}
+		</div>
+	);
 };
 
 function LogInToInstagram() {
-  const authURL =
-    "https://api.instagram.com/oauth/authorize?client_id=765093417767855&redirect_uri=https://localhost:3000/auth/&scope=user_profile,user_media&response_type=code";
+	const authURL =
+		'https://api.instagram.com/oauth/authorize?client_id=765093417767855&redirect_uri=https://localhost:3000/auth/&scope=user_profile,user_media&response_type=code';
 
-  window.location = authURL;
+	window.location = authURL;
+	// shell.openExternal(authURL);
 }
 
 export default InstagramSettings;
